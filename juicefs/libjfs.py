c10c3b76533ae78e8ea91044566b9a9795d0f9f8
HugoAhoy <zhjd3302@outlook.com>
2021-09-02 16:57:34 +0800

    revert read_cstring()

--- juicefs/libjfs.py
+++ juicefs/libjfs.py
@@ -12,13 +12,8 @@ from typing import Callable, List, Optional
 from juicefs.utils import check_juicefs_error
 
 
-def read_cstring(buffer: BytesIO, size: Optional[int] = None) -> bytearray:
+def read_cstring(buffer: BytesIO) -> bytearray:
     res = bytearray()
-    if size is not None:
-        for i in range(size):
-            res.append(ord(buffer.read(1)))
-        return res
-
     while True:
         b = buffer.read(1)
         if not b:
